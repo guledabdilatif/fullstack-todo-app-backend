bf426b1448464e744fa064689617cd004f234c40
guledabdilatif <guledabdilatif@gmail.com>
2025-02-07 11:19:21 -0800

    updated index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: fullstack-todo-app-backend-steel.vercel.app");
+header("Access-Control-Allow-Origin: https://fullstack-todo-app-backend-steel.vercel.app");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
